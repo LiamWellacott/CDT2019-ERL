fd9921e47aaa372763cbb11b076674bba9de87ae
Pierre Nicolay <nicolaypierre.95@gmail.com>
2021-02-24 17:49:55 +0000

    service call and expo to main with dummy nlp

--- nlp/scripts/nlp.py
+++ nlp/scripts/nlp.py
@@ -11,6 +11,7 @@ from flask import Flask, render_template
 
 from flask_ask import Ask, statement, question, session
 
+from erl_msgs.srv import Speech, SpeechResponse, Command
 
 app = Flask(__name__)
 
@@ -19,11 +20,24 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 rospy.init_node("nlp", anonymous=True)
+speachServ = rospy.Service('speech', handle_speech)
 rate = rospy.Rate(10)
 resp_pub = rospy.Publisher("/erl/nlp/resp", String, queue_size=10)
 cmd_pub = rospy.Publisher("/erl/nlp/command", String, queue_size=10)
 
+def handle_speech(req):
+    rospy.loginfo("Saying: " + req)
+    return SpeechResponse("Done")
 
+def cmd_service(msg):
+    rospy.wait_for_service('voc_cmd')
+    try:
+        voc_cmd_service = rospy.ServiceProxy('voc_cmd', Command)
+        ret = voc_cmd_service(msg)
+        return ret
+    except rospy.ServiceException as e:
+         rospy.loginfo("Service call failed: %s"%e)
+         return "vocal command service failed"
 
 @ask.launch
 def new_game():
@@ -34,14 +48,16 @@ def new_game():
 
 @ask.intent("PlaceIntent", convert={"object": str}, default={"place": "bin"})
 def placeobj(object, place):
-    publish("Place object " + object + " in " + place, "None")
-    return
+    str=""
+    ret = cmd_service(str)
+    return statement(ret)
 
 
 @ask.intent("PickupIntent", convert={"object":str})
 def pickup(object):
-    publish("Pickup object " + object)
-    return
+    str=""
+    ret = cmd_service(str)
+    return statement(ret)
 
 
 
